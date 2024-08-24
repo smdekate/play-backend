b69a84d5209183b0eebc1b11c41723c8c8fcb407
smdekate <smdekate@gmail.com>
2024-08-24 12:19:44 +0530

    app configuration settings

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import dotenv from "dotenv";
 import connectDB from "./database/index.js";
+import app from "./app.js";
 dotenv.config();
 
+const port = process.env.PORT || 3000;
 
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log(`Application error :: ${error}`);
+        throw error;
+    })
 
-
-
-connectDB();
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`)
+    })
+})
+.catch((error) => {
+    console.log(`Database connection error :: ${error}`);
+    throw error;
+})
 
 
 
